d466d8a0b85380fe3c3152d7eb8414060b76c2bf
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-10-19 10:23:07 +0500

    Added ReactTable component

---
+++ src/components/ReactTable/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import type {ColumnDef} from "@tanstack/react-table";
+import {flexRender, getCoreRowModel, useReactTable, getSortedRowModel} from "@tanstack/react-table";
+
+
+type ReactTableProps<D extends unknown> = {
+    data: D[];
+    colums: ColumnDef<D, string>[];
+    headerProps?: {};
+    headerCellProps?: {};
+    bodyProps?: {};
+    rowDataProps?: {className: string};
+    cellProps?: {className: string};
+    size?: keyof typeof sizes;
+
+    [x: string]: any;
+};
+const sizes = {
+    xs: "py-[30px]"
+} as const;
+
+const ReactTable = <D extends unknown>({
+    columns,
+    data = [],
+    headerProps = {},
+    headerCellProps = {},
+    bodyProps = {},
+    className = "",
+    rowDataProps = {className: ""},
+    cellProps = {className: ""},
+    size,
+    ...restConfig
+}: ReactTableProps<D>) => {
+    const tableConfig = {
+        columns, data,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        ...restConfig
+    };
+    const table = useReactTable(tableConfig);
+
+    return (
+        <table className={className}>
+            <thead {...headerProps}>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                            <th key={header.id} {...header.column.columnDef?.meta} {...headerCellProps}>
+                                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...bodyProps}>
+                {table.getRowModel().rows.map((row:any) => (<tr {...rowDataProps} className={`${rowDataProps?.className}`} key={row.id}>
+                    {row.getVisibleCells().map((cell:any) => (
+                        <td key={cell.id} className={`${size ? sizes[size] : ""} ${cellProps?.className}`}>
+                            {flexRender(cell.column.ColumnDef.cell, cell.getContext())}
+                        </td>
+                    ))}
+                </tr>))}
+            </tbody>
+        </table>
+    )
+};
+
+export {ReactTable};
